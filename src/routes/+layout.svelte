826da9e58e374caa62b59d1c0acdfa0b0ee0d51f
Bob Fanger <bfanger@gmail.com>
2023-06-24 08:36:43 +0200

    chore: Updated deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,5 @@
-<slot />
+<script>
+  import "../app.scss";
+</script>
 
-<style>
-  :global(html),
-  :global(body) {
-    height: 100%;
-    padding: 0;
-    margin: 0;
-  }
-  :global(svelte-app) {
-    display: block;
-    height: 100%;
-  }
-</style>
+<slot />
