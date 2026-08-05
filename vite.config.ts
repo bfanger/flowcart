5685b2db34869ec48a59f42ec52432e5776efdd3
Bob Fanger <bfanger@gmail.com>
2026-08-05 23:03:33 +0200

    chore: Updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import { existsSync } from "node:fs";
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
 
 const config: UserConfig = {
   plugins: [sveltekit()],
+  server: existsSync("/.dockerenv")
+    ? { host: true, watch: { usePolling: true, interval: 1000 } }
+    : undefined,
 };
 
 export default config;
